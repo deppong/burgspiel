16f4dbc2a01918c09a31a238ace85ceb4056440b
Isaiah Deppong <isaiahjdeppong@gmail.com>
2026-02-25 12:14:18 -0500

    Added a small workaround to make it -1 when features and everything are empty

--- src/main.c
+++ src/main.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 #include <stdint.h>
 
+#define EMPTY -1
+
 typedef enum {
-    CITY=1,
+    CITY,
     ROAD,
     FARM
 } feat_type_t;
 
 // should probably make them so if you +1 mod 12 it loops over for easy rotating.
 typedef enum {
-    EMPTY, //hack. should be changed
     N, E, S, W,
     NW, NE, EN, ES,
     SE, SW, WS, WN
@@ -18,7 +19,7 @@ typedef struct {
     // type
     feat_type_t type;
     // flags
-    uint8_t touching_city, cloister;
+    int8_t touching_city, cloister;
     dir_t touching[9] // maximum 9 directions of specificity (farm on 3 edges);
 } feature_t;
 
@@ -29,23 +30,25 @@ typedef struct {
     feature_t features[9];
 } tile_t;
 
+
 int main() {
     // example starting tile
 
     tile_t starting_tile = {
         .edges = {CITY, ROAD, FARM, ROAD},
         .features = {
-            {.type = CITY, .touching = {N}},
-            {.type = ROAD, .touching = {E, W}},
-            {.type = FARM, .touching = {EN, WN}, .touching_city=1},
-            {.type = FARM, .touching = {ES, S, WS}, .touching_city=0}
+            {.type = CITY, .touching = {N, -1}},
+            {.type = ROAD, .touching = {E, W,-1}},
+            {.type = FARM, .touching = {EN, WN,-1}, .touching_city=1},
+            {.type = FARM, .touching = {ES, S, WS,-1}, .touching_city=0},
+            {-1}
         }
     };
 
     for(int i = 0; i < 9; i++) {
-        if (!starting_tile.features[i].type) break;
+        if (starting_tile.features[i].type == EMPTY) break;
         for(int j = 0; j < 9; j++) {
-            if(!starting_tile.features[i].touching[j]) break;
+            if(starting_tile.features[i].touching[j] == EMPTY) break;
             printf("%d %d\n", starting_tile.features[i].type, starting_tile.features[i].touching[j]);
         }
     }
