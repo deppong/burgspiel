689a47f3e2704f3016ffa3f0ea251e0b98963ed0
Isaiah Deppong <isaiahjdeppong@gmail.com>
2026-02-25 12:56:19 -0500

    factored out tile_t struct info to tile.h

--- src/main.c
+++ src/main.c
@@ -1,39 +1,8 @@
 #include <stdio.h>
-#include <stdint.h>
-
-#define EMPTY -1
-
-typedef enum {
-    CITY,
-    ROAD,
-    FARM
-} feat_type_t;
-
-// should probably make them so if you +1 mod 12 it loops over for easy rotating.
-typedef enum {
-    N, E, S, W,
-    NW, NE, EN, ES,
-    SE, SW, WS, WN
-} dir_t;
-typedef struct {
-    // type
-    feat_type_t type;
-    // flags
-    int8_t touching_city, cloister;
-    dir_t touching[9] // maximum 9 directions of specificity (farm on 3 edges);
-} feature_t;
-
-typedef struct {
-    // always in the order of NESW
-    feat_type_t edges[4];
-     // Maximum 9 features (quad road tile)
-    feature_t features[9];
-} tile_t;
-
+#include "tile.h"
 
 int main() {
     // example starting tile
-
     tile_t starting_tile = {
         .edges = {CITY, ROAD, FARM, ROAD},
         .features = {
